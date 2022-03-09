b1bb8d668c45eab763e75d83a73ec72e939fe755
Melih <melih.kara@kit.edu>
2022-03-09 10:34:29 +0100

    allow for loading models externally

--- multimessenger/supernova/Supernova_Models.py
+++ multimessenger/supernova/Supernova_Models.py
@@ -58,14 +58,15 @@ class SN_LightCurve:
     """
 
     def __init__(self,
-                 progenitor_mass,
-                 metallicity,
-                 time_of_revival,
+                 # progenitor_mass,
+                 # metallicity,
+                 # time_of_revival,
                  distance=10,
                  recoil_energies=(0, 20, 50),
-                 filename_=None,
+                 # filename_=None,
                  storage=None,
-                 force=False):
+                 # force=False
+                 ):
         """
         Parameters
         ----------
@@ -85,13 +86,13 @@ class SN_LightCurve:
 
         """
         self.XeNuc = get_composite()
-        self.M = progenitor_mass
-        self.t_revival = time_of_revival
-        self.Z = metallicity
+        # self.M = progenitor_mass
+        # self.t_revival = time_of_revival
+        # self.Z = metallicity
         self.dist = distance
         self.t0 = 0
         self.tf = 10
-        self.name = filename_ or f'Object_M{self.M}-Z{self.Z}_dist{self.dist}.p'
+        # self.name = filename_ or f'Object_{self.M}-M_-{self.Z}-Z_{self.t_revival}-t_dist{self.dist}.p'
         self.recoil_en = np.linspace(recoil_energies[0], recoil_energies[1], recoil_energies[2])
         if storage is None:
             try:
@@ -103,21 +104,21 @@ class SN_LightCurve:
                 self.storage = os.getcwd()
         else:
             self.storage = storage
-        self.__version__ = "0.0.5"
+        self.__version__ = "0.1.5"
 
-        if force:
-            print('Running.. Saving the Object..\n')
-            self._load_light_curve()
-            self.save_object(update=True)
-        else:
-            try:
-                self.retrieve_object()
-                print('Object was found! \n'
-                      'To save manually: save_object(filename, update=True)\n')
-            except:
-                print('Running.. Saving the Object..\n')
-                self._load_light_curve()
-                self.save_object(update=True)
+        # if force:
+        #     print('Running.. Saving the Object..\n')
+        #     self._load_light_curve()
+        #     self.save_object(update=True)
+        # else:
+        #     try:
+        #         self.retrieve_object()
+        #         print('Object was found! \n'
+        #               'To save manually: save_object(filename, update=True)\n')
+        #     except:
+        #         print('Running.. Saving the Object..\n')
+        #         self._load_light_curve()
+        #         self.save_object(update=True)
 
     def save_object(self, filename=None, update=False):
         """
@@ -139,76 +140,155 @@ class SN_LightCurve:
         self.__dict__.update(tmp_dict.__dict__)
         return None
 
-    def _load_light_curve(self):
-        """ The data are taken from http://asphwww.ph.noda.tus.ac.jp/snn/
-        Load data from file;
-        File structure is
-        `    time_step,\n
-             col1 col2 col3 col4 col5 col6 \n
-        20x  col1 col2 col3 col4 col5 col6 \n
-             col1 col2 col3 col4 col5 col6 \n
-        `
-        This function is way slower than Ricardo Peres's, but more readable
+    def load_from_X(self, filename=None, *args):
+        """ As long as it returns
+            neutrino_properties = (time, mean_E, Nve, Nave, Nvx, Lve, Lave, Lvx)
+            and afterwards make lightcurve attributes
         """
-        mass = self.M
-        Z = self.Z
-        t_revival = self.t_revival
-        available_mass = [13, 20, 30, 50]
-        available_Z = [0.02, 0.004]
-        available_t_revival = [100, 200, 300]
-        assert mass in available_mass, "Required mass not in DB."
-        assert Z in available_Z, "Required metallicity not in DB."
-        assert t_revival in available_t_revival, "Required revival time not in DB."
+        from .model_loader import _load_from_X
+        neutrino_properties = _load_from_X(*args)
+        # self.name = filename
+        # self._make_lc_attributes(*neutrino_properties)
+        # self.save_object(update=True)
+        pass
 
-        B = 0
-        if Z == 0.004: B = 1
-        C = int(t_revival / 100)
+    def load_model_from_db(self, progenitor_mass, metallicity, time_of_revival,
+                           filename=None, path=None, force=False):
+        """
+        Parameters:
+        ----------
+            progenitor_mass : Mass of the progenitor
+            metallicity: metallicity of the progenitor
+            time_of_revival : time of revival of the model
+            filename : optional filename, by default it is unique for the model
+            path : model path, optional, default is fetched from config
 
-        try:
-            # filepath = f'{paths["data"]}intp{mass}{B}{C}.data'
-            filepath = f'../data/rperes_data/intp{mass}{B}{C}.data'
-            _f = open(filepath, 'r')
-        except:
-            filepath = f'/dali/lgrandi/peres/SN/Light_Curve_DB/intpdata/intp{mass}{B}{C}.data'
-            _f = open(filepath, 'r')
-        f = _f.readlines()
-        _f.close()
-        # The 0th line is the time
-        # following 20 lines are the data
-        # there is an empty line and a line for next time step after every 20 lines
-        skiprows = [0]
-        for i in range(int(len(f) / 22)):
-            skiprows.append(21 + i * 22)
-            skiprows.append(22 + i * 22)
+        Returns
+        -------
+        `tuple` (time, mean_E, Nve, Nave, Nvx, Lve, Lave, Lvx)
+        """
+        from .model_loader import _load_light_curve as loader
+        self.M = progenitor_mass
+        self.t_revival = time_of_revival
+        self.Z = metallicity
+        self.name = filename or f'Object_{self.M}-M_-{self.Z}-Z_{self.t_revival}-t_dist{self.dist}.p'
 
-        # make a file without the empty lines or time lines
-        f_filt = [fval for fval in f if f.index(fval) not in skiprows]
-        f_new = open('tmp', 'w+')
-        f_new.writelines(f_filt)
-        f_new.close()
-        # remove tmp if you like
+        if force:
+            print('Running.. Saving the Object..\n')
+            args = loader(progenitor_mass, metallicity, time_of_revival, path)
+        else:
+            try:
+                self.retrieve_object(self.name)
+                click.secho('Object was found! \n'
+                            'To save manually: save_object(filename, update=True)\n', fg='green')
+                return None
+            except:
+                click.secho('Running.. Saving the Object..\n', fg='blue')
+                args = loader(progenitor_mass, metallicity, time_of_revival, path)
+        self._make_lc_attributes(*args)
+        self.save_object(update=True)
 
-        time = np.array([float(fval.split('\n')[0]) for fval in f if f.index(fval) in skiprows and fval != ' \n'])
-        E_bins_left, E_bins_right, N_ve, N_ave, N_vx, L_ve, L_ave, L_vx = np.loadtxt('tmp', unpack=True)
-        row_nr = int(len(L_vx) / 20)
-        # the energies are same at a given time index
-        self.t = time  # *u.s
-        self.E_bins_left = E_bins_left.reshape(row_nr, 20)[0, :]  # * u.MeV
-        self.E_bins_right = E_bins_right.reshape(row_nr, 20)[0, :]  # * u.MeV
-        self.N_ve = N_ve.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
-        self.N_ave = N_ave.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
-        self.N_vx = 4 * N_vx.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
-        self.L_ve = L_ve.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
-        self.L_ave = L_ave.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
-        self.L_vx = L_vx.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
-        self.nu_list = {r'$\nu_e$': self.N_ve,
-                        r'$\overline{\nu_e}$': self.N_ave,
-                        r'$\sum\nu_x$': self.N_vx}
-        self.L_list = {r'L$\nu_e$': self.L_ve,
-                       r'L$\overline{\nu_e}$': self.L_ave,
-                       r'L$\nu_x$': self.L_vx}
-        self.mean_E = (self.E_bins_right + self.E_bins_left) / 2
-        return None
+
+    # def _load_light_curve(self, path=None):
+    #     """ The data are taken from http://asphwww.ph.noda.tus.ac.jp/snn/
+    #     Load data from file;
+    #     File structure is
+    #     `    time_step,\n
+    #          col1 col2 col3 col4 col5 col6 \n
+    #     20x  col1 col2 col3 col4 col5 col6 \n
+    #          col1 col2 col3 col4 col5 col6 \n
+    #     `
+    #     This function is way slower than Ricardo Peres's, but more readable
+    #     """
+    #     mass = self.M
+    #     Z = self.Z
+    #     t_revival = self.t_revival
+    #     available_mass = [13, 20, 30, 50]
+    #     available_Z = [0.02, 0.004]
+    #     available_t_revival = [100, 200, 300]
+    #     assert mass in available_mass, "Required mass not in DB."
+    #     assert Z in available_Z, "Required metallicity not in DB."
+    #     assert t_revival in available_t_revival, "Required revival time not in DB."
+    #
+    #     B = 0
+    #     if Z == 0.004:
+    #         B = 1
+    #     C = int(t_revival / 100)
+    #
+    #     default_path = f'/dali/lgrandi/peres/SN/Light_Curve_DB/intpdata/'
+    #     dataname = f"intp{mass}{B}{C}.data"
+    #     if path is not None:
+    #         filepath = os.path.join(path,dataname)
+    #     else:
+    #         filepath = os.path.join(default_path, dataname)
+    #     _f = open(filepath, 'r')
+    #     f = _f.readlines()
+    #     _f.close()
+    #     # The 0th line is the time
+    #     # following 20 lines are the data
+    #     # there is an empty line and a line for next time step after every 20 lines
+    #     skiprows = [0]
+    #     for i in range(int(len(f) / 22)):
+    #         skiprows.append(21 + i * 22)
+    #         skiprows.append(22 + i * 22)
+    #
+    #     # make a file without the empty lines or time lines
+    #     f_filt = [fval for fval in f if f.index(fval) not in skiprows]
+    #     f_new = open('tmp', 'w+')
+    #     f_new.writelines(f_filt)
+    #     f_new.close()
+    #     # remove tmp if you like
+    #
+    #     time = np.array([float(fval.split('\n')[0]) for fval in f if f.index(fval) in skiprows and fval != ' \n'])
+    #     E_bins_left, E_bins_right, N_ve, N_ave, N_vx, L_ve, L_ave, L_vx = np.loadtxt('tmp', unpack=True)
+    #     row_nr = int(len(L_vx) / 20)
+    #     # the energies are same at a given time index
+    #     # self.t = time  # *u.s
+    #     E_bins_left = E_bins_left.reshape(row_nr, 20)[0, :]  # * u.MeV
+    #     E_bins_right = E_bins_right.reshape(row_nr, 20)[0, :]  # * u.MeV
+    #     mean_E = (E_bins_right + E_bins_left) / 2
+    #
+    #     Nve = N_ve.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
+    #     Nave = N_ave.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
+    #     Nvx = 4 * N_vx.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
+    #     Lve = L_ve.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
+    #     Lave = L_ave.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
+    #     Lvx = L_vx.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
+    #     return time, mean_E, Nve, Nave, Nvx, Lve, Lave, Lvx
+
+    def _make_lc_attributes(self, time, nu_energies, Nve, Nave, Nvx, Lve, Lave, Lvx):
+        """ make object attributes using params
+        Parameters
+        ----------
+        time : `np.array`
+            neutrino arrival times of shape N, unit sec
+        nu_energies : `array`
+            neutrino mean energies of shape M, unit MeV
+        Nve, Nave, Nvx : `array`
+            electron, anti-electron, and other type neutrino fluxes
+            with unit (count*MeV**-1 * s**-1)
+        Lve, Lave, Lvx : `array`
+            electron, anti-electron, and other type neutrino luminosities
+            with unit (erg * MeV**-1 * s**-1)
+
+        Returns
+        -------
+        appends following attributes
+        self.t = time bins of neutrinos of shape (N,)
+        self.mean_E = mean neutrino energies (computed from E_bins_r/l) of shape (M,)
+        self.nu_list = dictionary containing Nve, Nave, Nvx
+        self.L_list = dictionary containing Lve, Lave, Lvx
+        """
+        self.t = time
+        self.mean_E = nu_energies
+        self.nu_list = {r'$\nu_e$': Nve,
+                        r'$\overline{\nu_e}$': Nave,
+                        r'$\sum\nu_x$': Nvx}
+        self.L_list = {r'L$\nu_e$': Lve,
+                       r'L$\overline{\nu_e}$': Lave,
+                       r'L$\nu_x$': Lvx}
+
+
 
     def _get_t_indexes(self, t0=None, tf=None):
         """ find the closest indexes of t0 and tf """
---
+++ multimessenger/supernova/model_loader.py
@@ -0,0 +1,76 @@
+
+import os
+import numpy as np
+
+
+def _load_from_X(*args):
+    """ As long as it returns
+        args = (time, mean_E, Nve, Nave, Nvx, Lve, Lave, Lvx)
+    """
+    # args = somestuff()
+    raise NotImplementedError
+
+
+def _load_light_curve(mass, Z, t_revival, path=None):
+    """ The data are taken from http://asphwww.ph.noda.tus.ac.jp/snn/
+    Load data from file;
+    File structure is
+    `    time_step,\n
+         col1 col2 col3 col4 col5 col6 \n
+    20x  col1 col2 col3 col4 col5 col6 \n
+         col1 col2 col3 col4 col5 col6 \n
+    `
+    This function is way slower than Ricardo Peres's, but more readable
+    """
+    available_mass = [13, 20, 30, 50]
+    available_Z = [0.02, 0.004]
+    available_t_revival = [100, 200, 300]
+    assert mass in available_mass, "Required mass not in DB."
+    assert Z in available_Z, "Required metallicity not in DB."
+    assert t_revival in available_t_revival, "Required revival time not in DB."
+
+    B = 0
+    if Z == 0.004:
+        B = 1
+    C = int(t_revival / 100)
+
+    default_path = f'/dali/lgrandi/peres/SN/Light_Curve_DB/intpdata/'
+    dataname = f"intp{mass}{B}{C}.data"
+    if path is not None:
+        filepath = os.path.join(path ,dataname)
+    else:
+        filepath = os.path.join(default_path, dataname)
+    _f = open(filepath, 'r')
+    f = _f.readlines()
+    _f.close()
+    # The 0th line is the time
+    # following 20 lines are the data
+    # there is an empty line and a line for next time step after every 20 lines
+    skiprows = [0]
+    for i in range(int(len(f) / 22)):
+        skiprows.append(21 + i * 22)
+        skiprows.append(22 + i * 22)
+
+    # make a file without the empty lines or time lines
+    f_filt = [fval for fval in f if f.index(fval) not in skiprows]
+    f_new = open('tmp', 'w+')
+    f_new.writelines(f_filt)
+    f_new.close()
+    # remove tmp if you like
+
+    time = np.array([float(fval.split('\n')[0]) for fval in f if f.index(fval) in skiprows and fval != ' \n'])
+    E_bins_left, E_bins_right, N_ve, N_ave, N_vx, L_ve, L_ave, L_vx = np.loadtxt('tmp', unpack=True)
+    row_nr = int(len(L_vx) / 20)
+    # the energies are same at a given time index
+    # self.t = time  # *u.s
+    E_bins_left = E_bins_left.reshape(row_nr, 20)[0, :]  # * u.MeV
+    E_bins_right = E_bins_right.reshape(row_nr, 20)[0, :]  # * u.MeV
+    mean_E = (E_bins_right + E_bins_left) / 2
+
+    Nve = N_ve.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
+    Nave = N_ave.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
+    Nvx = 4 * N_vx.reshape(row_nr, 20)  # * (u.count*u.MeV**-1 * u.s**-1)
+    Lve = L_ve.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
+    Lave = L_ave.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
+    Lvx = L_vx.reshape(row_nr, 20)  # * (u.erg * u.MeV**-1 * u.s**-1)
+    return time, mean_E, Nve, Nave, Nvx, Lve, Lave, Lvx
