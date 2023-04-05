26bfd2724625f99379e8dd3f030f7a793c8054cc
Melih Kara <kara@kit.edu>
2023-04-05 11:48:27 +0200

    fix typo

--- scripts/submit_sim.py
+++ scripts/submit_sim.py
@@ -19,7 +19,7 @@ parser.add_argument('-i', '--model_index',
                     type=str, required=True)
 parser.add_argument('-rib', '--runidbase', help='runid base.', type=str, required=True)
 parser.add_argument('-N', '--ntotal', help='Number of realizations to simulate. For N=M, simulates the model M times.',
-                    type=int, defaul=1, required=False)
+                    type=int, default=1, required=False)
 parser.add_argument('-d', '--distance', help='Distance to SN in kpc.', default = 10, type=float)
 parser.add_argument('-v', '--volume', help='Volume in tons.', default = 5.9, type=float)
 
@@ -34,7 +34,7 @@ distance = args.distance
 volume = args.volume
 runidbase = args.runidbase
 
-def make_batch_script(model_name,model_index,distance,volume,runid, incr):
+def make_batch_script(config, model_name,model_index,distance,volume,runid, incr):
     main_str = f'''#!/bin/bash
 #SBATCH --qos=xenon1t
 #SBATCH --partition=xenon1t
@@ -54,7 +54,7 @@ singularity shell \\
     --bind /scratch/midway2/$USER \\
     --bind /dali \\
     /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg <<EOF
-python simulate_snmodel.py -m {model_name} -i {model_index} -d {distance} -v {volume} -id {runid}
+python simulate_snmodel.py -c {config} -m {model_name} -i {model_index} -d {distance} -v {volume} -id {runid}
 EOF''' 
     with open(f'SNsim_{runid}_{incr}.job', 'w') as F:
         F.write(main_str)  
@@ -65,5 +65,5 @@ if __name__ == "__main__":
     print('Making .job file:')
     for i in range(ntotal):
         incr = f"{i:03}"
-        make_batch_script(model_name, model_index, distance, volume, runidbase, incr)
+        make_batch_script(config_file, model_name, model_index, distance, volume, runidbase, incr)
         os.system(f"sbatch SNsim_{runidbase}_{incr}.job")
