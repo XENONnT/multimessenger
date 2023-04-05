7d52347174bc8cb5d267f631c431bf9ae704545b
Melih Kara <kara@kit.edu>
2023-04-05 09:55:00 +0200

    change batch-job scripts

--- scripts/simulate_snmodel.py
+++ scripts/simulate_snmodel.py
@@ -1,26 +1,27 @@
 import argparse
-from multimessenger.supernova import Supernova_Models as sn
+from multimessenger.supernova import Supernova_Models
+from multimessenger.supernova.interactions import Interactions
 import numpy as np
-import nestpy
-import pandas as pd
+import cutax
 import straxen
-import astropy.units as u
+
 
 parser = argparse.ArgumentParser(
     description=('Script to run a SN simulation of a given model.')
 )
+
+parser.add_argument('-c', '--config',
+                    help=('The config file with the data paths'),
+                    type=str,
+                    required=True)
 parser.add_argument('-m', '--model',
                     help='Model to simulate, call --help for list of models.',
-                    choices = sn.models_list,
+                    choices = Supernova_Models.models_list,
                     type=str,
                     required=True)
 parser.add_argument('-i', '--model_index',
                     help=('Some model types have differnt options. Select them by'
                           'index. TO BE IMPROVED.'),
-                    type=int,
-                    required=True)
-parser.add_argument('-N', '--ntotal',
-                    help='Number of events to simulate. -1 for realistic simulation.',
                     type=int,
                     required=True)
 parser.add_argument('-d', '--distance',
@@ -40,47 +41,39 @@ args = parser.parse_args()
 
 downloader = straxen.MongoDownloader()
 
+config_file = args.config
 model_name = args.model
 model_index = args.model_index
-ntotal = args.ntotal
 distance = args.distance
 volume = args.volume
 runid = args.runid
 
+def get_model(config_file, model_name, index, distance):
+    SelectedModel = Supernova_Models.Models(model_name, config_file=config_file)
+    SelectedModel(index=index)  # brings the attributes
+    SelectedModel.compute_model_fluxes(neutrino_energies=np.linspace(0, 50, 200))
+    _ = SelectedModel.scale_fluxes(distance=distance)
+    return SelectedModel
+
+def get_interactions(SelectedModel, distance, volume):
+    Int = Interactions(SelectedModel, Nuclei='Xenon', isotope='mix')  # create interactions
+    Int.compute_interaction_rates()  # compute rates
+    _ = Int.scale_rates(distance=distance, volume=volume)  # scale rates for dist & vol
+    return Int
+
+def fetch_context():
+    """ If context is updated, change it in here
+    """
+    return cutax.contexts.xenonnt_sim_SR0v4_cmt_v9(output_folder="/project2/lgrandi/xenonnt/simulations/supernova/")
+
+
 def main():
-    
-    A = sn.Models(model_name=model_name, index=model_index, distance=distance*u.kpc, volume=volume*u.t)
-    A.compute_rates() #fetches the already existing sim
+    straxen.print_versions("strax,straxen,cutax,wfsim".split(","))
+    SelectedModel = get_model(config_file, model_name, model_index, distance)
+    Interaction = get_interactions(SelectedModel, distance, volume)
+    context = fetch_context()
+    # simulate
+    Interaction.simulate_automatically(context=context, runid=runid)
 
-    nevents = int(A.single_rate.value)
-    
-    field_file="fieldmap_2D_B2d75n_C2d75n_G0d3p_A4d9p_T0d9n_PMTs1d3n_FSR0d65p_QPTFE_0d5n_0d4p.json.gz"
-    field_map = straxen.InterpolatingMap(
-                        straxen.get_resource(downloader.download_single(field_file),
-                                            fmt="json.gz"),
-                        method="RegularGridInterpolator")
-
-    nc = nestpy.NESTcalc(nestpy.DetectorExample_XENON10())
-    ## not sure if nestpy RNG issue was solved, so randomize NEST internal state
-    for i in range(np.random.randint(100)):
-        nc.GetQuanta(nc.GetYields(energy=np.random.uniform(10,100)))
-    
-    if ntotal == -1:
-        N_events = nevents
-        sampled_t = A.sample_data(nevents, dtype='time')
-    else:
-        N_events = ntotal
-        sampled_t = "shifted"
-        
-    sampled_Er = A.sample_data(N_events)
-    instr = A.generate_instructions(energy_deposition=sampled_Er, 
-                                    timemode=sampled_t, 
-                                    n_tot=N_events, 
-                                    nc=nc, 
-                                    fmap=field_map)
-    df = pd.DataFrame(instr)
-    print(f"Total duration {np.ptp(df['time'])*1e-9:.2f} seconds")
-    st = A.simulate_one(df, runid)
-    
 if __name__ == "__main__":
     main()
--- scripts/submit_sim.py
+++ scripts/submit_sim.py
@@ -1,68 +1,46 @@
 import argparse
 import os
-#from multimessenger.supernova import Supernova_Models as sn
-models_list = [
- 'Bollig_2016',
- 'Fornax_2019',
- 'Fornax_2021',
- 'Kuroda_2020',
- 'Nakazato_2013',
- 'OConnor_2013',
- 'OConnor_2015',
- 'Sukhbold_2015',
- 'Tamborra_2014',
- 'Walk_2018',
- 'Walk_2019',
- 'Warren_2020',
- 'Zha_2021']
+from multimessenger.supernova.snewpy_models import models_list
 
 parser = argparse.ArgumentParser(
     description=('Script to submit request a simulation of a given SN '
                  'model via batch job.')
 )
+
+parser.add_argument('-c', '--config',
+                    help=('The config file with the data paths'),
+                    type=str, required=True)
 parser.add_argument('-m', '--model',
                     help='Model to simulate, call --help for list of models.',
-                    choices = models_list,
-                    type=str,
-                    required=True)
+                    choices = models_list, type=str, required=True)
 parser.add_argument('-i', '--model_index',
-                    help=('Some model types have differnt options. Select them by'
+                    help=('Some model types have different options. Select them by'
                           'index. TO BE IMPROVED.'),
-                    type=str,
-                    required=True)
-parser.add_argument('-N', '--ntotal',
-                    help='Number of events to simulate. -1 for realistic simulation.',
-                    type=int,
-                    required=True)
-parser.add_argument('-d', '--distance',
-                    help='Distance to SN in kpc.',
-                    default = 10,
-                    type=float)
-parser.add_argument('-v', '--volume',
-                    help='Volume in tons.',
-                    default = 5.9,
-                    type=float)
-parser.add_argument('-id', '--runid',
-                    help='runid to consider.',
-                    type=str,
-                    required=True)
+                    type=str, required=True)
+parser.add_argument('-rib', '--runidbase', help='runid base.', type=str, required=True)
+parser.add_argument('-N', '--ntotal', help='Number of realizations to simulate. For N=M, simulates the model M times.',
+                    type=int, defaul=1, required=False)
+parser.add_argument('-d', '--distance', help='Distance to SN in kpc.', default = 10, type=float)
+parser.add_argument('-v', '--volume', help='Volume in tons.', default = 5.9, type=float)
+
 
 args = parser.parse_args()
 
+config_file = args.config
 model_name = args.model
 model_index = args.model_index
 ntotal = args.ntotal
 distance = args.distance
 volume = args.volume
-runid = args.runid
+runidbase = args.runidbase
 
-def make_batch_script(model_name,model_index,ntotal,distance,volume,runid):
+def make_batch_script(model_name,model_index,distance,volume,runid, incr):
     main_str = f'''#!/bin/bash
 #SBATCH --qos=xenon1t
 #SBATCH --partition=xenon1t
-#SBATCH --job-name=wfsim_{runid}
-#SBATCH --output=/dali/lgrandi/melih/mma/sim_logs/wfsim_{runid}.out
-#SBATCH --error=/dali/lgrandi/melih/mma/sim_logs/wfsim_{runid}.err
+#SBATCH --job-name={runid}_{incr}
+#SBATCH --output=/dali/lgrandi/melih/mma/sim_logs/{runid}_{incr}.out
+#SBATCH --error=/dali/lgrandi/melih/mma/sim_logs/{runid}_{incr}.err
 #SBATCH --account=pi-lgrandi
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -76,15 +54,16 @@ singularity shell \\
     --bind /scratch/midway2/$USER \\
     --bind /dali \\
     /project2/lgrandi/xenonnt/singularity-images/xenonnt-development.simg <<EOF
-python simulate_snmodel.py -m {model_name} -i {model_index} -N {ntotal} -d {distance} -v {volume} -id {runid}
+python simulate_snmodel.py -m {model_name} -i {model_index} -d {distance} -v {volume} -id {runid}
 EOF''' 
-    with open(f'SN_sim_{runid}_wfsim.job', 'w') as F:
+    with open(f'SNsim_{runid}_{incr}.job', 'w') as F:
         F.write(main_str)  
-    print(f'Generated file with ID: {runid}')
+    print(f'Generated file with ID: {runid}_{incr}')
     
 
 if __name__ == "__main__":
     print('Making .job file:')
-    make_batch_script(model_name,model_index,ntotal,distance,volume,runid)
-    print('Launching batch job:')
-    os.system(f"sbatch SN_sim_{runid}_wfsim.job")
+    for i in range(ntotal):
+        incr = f"{i:03}"
+        make_batch_script(model_name, model_index, distance, volume, runidbase, incr)
+        os.system(f"sbatch SNsim_{runidbase}_{incr}.job")
