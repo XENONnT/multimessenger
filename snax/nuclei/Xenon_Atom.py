eaa8e8e599f738ce11372a5d52306a23b827310d
KaraMelih <kara@kit.edu>
2025-01-14 08:44:58 +0100

    add missing xenon atom

---
+++ snax/nuclei/Xenon_Atom.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+# Reference: https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=Xe
+# J. S. Coursey, D. J. Schwab, J. J. Tsai, and R. A. Dragoset, 
+# Atomic Weights and Isotopic Compositions with Relative Atomic Masses, 
+# NIST Physical Measurement Laboratory
+
+Xe124 = {
+    'Type'		: 'Xe124',
+    'MassNum'	: 124,
+    'AtomicNum'	: 54,
+    'Mass'		: 123.9058920,
+    'Spin'		: 0,
+    'Fraction'	: 9.52E-4
+}
+
+Xe126 = {
+    'Type'		: 'Xe126',
+    'MassNum'	: 126,
+    'AtomicNum'	: 54,
+    'Mass'		: 125.9042983,
+    'Spin'		: 0,
+    'Fraction'	: 8.90E-4
+}
+
+Xe128 = {
+    'Type'		: 'Xe128',
+    'MassNum'	: 128,
+    'AtomicNum'	: 54,
+    'Mass'		: 127.9035310,
+    'Spin'		: 0,
+    'Fraction'	: 0.019102
+}
+
+Xe129 = {
+    'Type'		: 'Xe129',
+    'MassNum'	: 129,
+    'AtomicNum'	: 54,
+    'Mass'		: 128.9047808611,
+    'Spin'		: 0.5,
+    'Fraction'	: 0.264006
+}
+
+Xe130 = {
+    'Type'		: 'Xe130',
+    'MassNum'	: 130,
+    'AtomicNum'	: 54,
+    'Mass'		: 129.903509349,
+    'Spin'		: 0,
+    'Fraction'	: 0.040710
+}
+
+Xe131 = {
+    'Type'		: 'Xe131',
+    'MassNum'	: 131,
+    'AtomicNum'	: 54,
+    'Mass'		: 130.90508406,
+    'Spin'		: 1.5,
+    'Fraction'	: 0.212324
+}
+
+Xe132 = {
+    'Type'		: 'Xe132',
+    'MassNum'	: 132,
+    'AtomicNum'	: 54,
+    'Mass'		: 131.9041550856,
+    'Spin'		: 0,
+    'Fraction'	: 0.269086
+}
+
+Xe134 = {
+    'Type'		: 'Xe134',
+    'MassNum'	: 134,
+    'AtomicNum'	: 54,
+    'Mass'		: 133.90539466,
+    'Spin'		: 0,
+    'Fraction'	: 0.104357
+}
+
+Xe136 = {
+    'Type'		: 'Xe136',
+    'MassNum'	: 136,
+    'AtomicNum'	: 54,
+    'Mass'		: 135.907214484,
+    'Spin'		: 0,
+    'Fraction'	: 0.088573
+}
+
+ATOM_TABLE = {
+    'Xe124'	: Xe124,
+    'Xe126'	: Xe126,
+    'Xe128'	: Xe128,
+    'Xe129'	: Xe129,
+    'Xe130'	: Xe130,
+    'Xe131'	: Xe131,
+    'Xe132'	: Xe132,
+    'Xe134'	: Xe134,
+    'Xe136'	: Xe136
+}
